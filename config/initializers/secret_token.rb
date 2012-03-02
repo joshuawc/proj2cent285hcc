08ffe50b90723ac88956275aa7ee7255e3bff5b5
Joshua Corpuz <corpuz.joshua@gmail.com>
2012-03-01 14:59:25 -1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studemo::Application.config.secret_token = '70d826077f02f140e84417e6ce79758adcd1b245c8e7161e835ed2d494edf720779a86c5e572bc723025882938c52efaf54249570c4d76233d028e815f400451'
